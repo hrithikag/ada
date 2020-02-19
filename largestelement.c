3f6e892efe90e4e1b49f5a369acf08f8d9092907
hrithikag <54281752+hrithikag@users.noreply.github.com>
2020-02-19 12:40:32 +0530

    Add files via upload

---
+++ largestelement.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void Bubble_Sort(int A[],int n)
+{
+	int temp;
+	int i,j;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-1-i;j++)
+		{
+			if(A[j]<A[j+1])
+			{
+				temp = A[j];
+				A[j] = A[j+1];
+				A[j+1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int A[10000];
+	int i,n;
+	int k;
+	printf("\nEnter The Size Of The Array : ");
+	scanf("%d",&n);
+	printf("\nEnter %d Elements : \n",n);
+	for(i=0;i<n;i++)
+		scanf("%d",&A[i]);
+	Bubble_Sort(A,n)	;
+	printf("\nEnter The K Value : ");
+	scanf("%d",&k);
+	if(k>n || k<=0)
+	{
+		printf("\nEnter The K Value Between 1 & %d\n",n);
+		exit(0);
+	}
+	else
+	{
+		printf("\nThe %d Largest Elements in The Array is : \n",k);
+		for(i=0;i<k;i++)
+		{
+			printf("\t%d",A[i]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
